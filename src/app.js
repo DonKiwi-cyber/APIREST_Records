fa6aa68a68e7ee3ee5253ae70cc5cf98e5118b97
Ian <minecraft200294@gmail.com>
2023-04-18 09:13:20 -0600

    updated

--- src/app.js
+++ src/app.js
@@ -10,12 +10,5 @@ app.use(express.json());
 // Uso de las rutas (Endpoints)
 app.use('/api',recordRoutes); 
 
-// Mensaje en caso de que los endpoints sean erroneos
-app.use((req, res, next) => {
-    req.status(404).json({
-        message: 'Endpoint no encontrado'
-    });
-});
-
 //Exportación de la aplicación Express a Index.js
 export default app;
